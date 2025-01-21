99870ffd93ef654dd15bc763f62403b26b08d2e9
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2025-01-21 12:56:14 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,19 +6,19 @@ if Path("config.env").exists():
     load_dotenv("config.env")
 
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "28836709"))
+    API_HASH = getenv("API_HASH", "7141c6095cd69aa0278daa12faa0cbad")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7790882826:AAFztT-k0YF_u55C1fiDSUgvgr9vJXnDe60")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "")
     BASE_URL = getenv("BASE_URL", "").rstrip('/')
-    DATABASE_URL = getenv("DATABASE_URL", "")
-    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "").split(",") if channel.strip()]
+    DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://moviebuzz203:TMFeRZa294fueVah@thiraiwebtg.h86nh.mongodb.net/?retryWrites=true&w=majority&appName=ThiraiWebTG")
+    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002342014518").split(",") if channel.strip()]
     THEME = getenv("THEME", "quartz").lower()
     USERNAME = getenv("USERNAME", "admin")
     PASSWORD = getenv("PASSWORD", "admin")
-    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "surfTG")
-    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
+    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "ThiraiTG")
+    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "ThiraiTG")
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '10'))
     MULTI_CLIENT = getenv('MULTI_CLIENT', 'False')
