a88406a1489f498a035fdeef6feb7e78f3936fb3
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2025-01-21 14:57:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     API_HASH = getenv("API_HASH", "7141c6095cd69aa0278daa12faa0cbad")
     BOT_TOKEN = getenv("BOT_TOKEN", "7790882826:AAFztT-k0YF_u55C1fiDSUgvgr9vJXnDe60")
     PORT = int(getenv("PORT", 8080))
-    SESSION_STRING = getenv("SESSION_STRING", "")
+    SESSION_STRING = getenv("SESSION_STRING", "BQG4A2UAIlONdiWehstmVrr5BID-D2Y8Bu8ciZoApsjBRCiJBcCMTqhkJvAzcEn0CfUGKwc4GQGWSFvoSAayGChthcfLzrjQwDEHXTPw8sDOfpkb_jFELMm5a91UW6jEUYgwN7zyKQe34h7gmAF5zLf-74jlXEGLtaKv90ygqqjpLfTpDLptZiD_ebavF-xUPjK1cbowwA2ZAjkW79_FeB056j8g9g3KDdIshn1bhA7udbv2XU-O6MRmmFJ1XdBW6EP2IXB-kV-uj30I3tlMk4otyw7kHoPrHIbc8H8V6tszkwpO0Bby11yRdBs1ZO8fYPymCdU_kc2Ro6ZwuLvtDLFA_9zAHAAAAAHQX3AKAQ")
     BASE_URL = getenv("BASE_URL", "https://thiraitg.koyeb.app/").rstrip('/')
     DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://thiariweb:thiariweb@thiraiwebtg.h86nh.mongodb.net/?retryWrites=true&w=majority")
     AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002342014518").split(",") if channel.strip()]
